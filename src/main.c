0b57565d09e538ffeb661cd26456be4b8e626fe5
mgoldenberg <mgoldenberg6@gmail.com>
2018-08-25 12:42:29 -0400

    fix some bugs, passing all tests

--- src/main.c
+++ src/main.c
@@ -9,24 +9,26 @@
 
 int main(int argc, char **argv) {
 	int (*b64_fcn)(const uint8_t *, const size_t, uint8_t *, const size_t);
-	int factor;
+	double factor;
 
 	char c;
 	while((c = getopt(argc, argv, "de")) != -1) {
 		switch(c) {
 			case 'd': /* decode */
 				b64_fcn = &b64_decode;
-				factor = 3 / 4;
+				factor = 1;
 				break;
 			case 'e': /* encode */
 				b64_fcn = &b64_encode;
-				factor = 4 / 3;
+				factor = 2;
+				break;
 			default:
 				abort();
 		}
 	}
 	if(b64_fcn == NULL) {
 		b64_fcn = &b64_encode;
+		factor = 2;
 	}
 
 	uint8_t *in = NULL;
@@ -34,18 +36,19 @@ int main(int argc, char **argv) {
 	ssize_t nread;
 	while((nread = getline((char **)&in, &size, stdin)) != -1) {
 		size_t inlen = nread;
-		size_t outlen = (inlen * factor) + 1;
+		size_t outlen = (inlen * factor) > 4 ? (inlen * factor) : 4;
 
 		uint8_t *out;
 		if((out = (uint8_t *)malloc(sizeof(uint8_t) * outlen)) == NULL) {
 			fprintf(stderr, "error: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		}
-		if(b64_fcn(in, inlen, out, outlen) == -1) {
+		int nwrit;
+		if((nwrit = b64_fcn(in, inlen, out, outlen)) == -1) {
 			fprintf(stderr, "error: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		}
-		printf("%.*s", outlen, out);
+		printf("%.*s", nwrit, out);
 		free(out);
 	}
 	free(in);
